561bc3f33249d13ee72ff668cbc4346fdf515a1f
LukeFZ <lukefinn@web.de>
2021-01-10 12:34:42 +0100

    Add project files.

---
+++ ProjectEarthServerAPI/Controllers/LocatorController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ProjectEarthServerAPI.Models;
+
+namespace ProjectEarthServerAPI.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("1.1")]
+    [Route("api/v{version:apiVersion}/player/environment")]
+    public class LocatorController : ControllerBase
+    {
+
+        private readonly ILogger<LocatorController> _logger;
+
+        public LocatorController(ILogger<LocatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            LocatorResponse.Root response = new LocatorResponse.Root()
+            {
+                result = new LocatorResponse.Result()
+                {
+                    serviceEnvironments = new LocatorResponse.ServiceEnvironments()
+                    {
+                        production = new LocatorResponse.Production()
+                        {
+                            cdnUri = "192.168.2.100",
+                            serviceUri = "192.168.2.100",
+                            playfabTitleId = "11509"
+                        }
+                    },
+                    supportedEnvironments = new LocatorResponse.SupportedEnvironments()
+                    {
+                        _2020121702 = new List<string>()
+
+                    }
+                },
+                continuationToken = null,
+                expiration = null,
+                updates = new LocatorResponse.Updates()
+            };
+            response.result.supportedEnvironments._2020121702.Add("int");
+            var send = JsonConvert.SerializeObject(response);
+            return send;
+
+        }
+    }
+}
---
+++ ProjectEarthServerAPI/Models/LocatorResponse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ProjectEarthServerAPI.Models
+{
+    public class LocatorResponse
+    {
+        public class Production
+        {
+            public string serviceUri { get; set; }
+            public string cdnUri { get; set; }
+            public string playfabTitleId { get; set; }
+        }
+
+        public class ServiceEnvironments
+        {
+            public Production production { get; set; }
+        }
+
+        public class SupportedEnvironments
+        {
+            [JsonProperty("2020.1217.02")]
+            public List<string> _2020121702 { get; set; }
+        }
+
+        public class Result
+        {
+            public ServiceEnvironments serviceEnvironments { get; set; }
+            public SupportedEnvironments supportedEnvironments { get; set; }
+        }
+
+        public class Updates
+        {
+        }
+
+        public class Root
+        {
+            public Result result { get; set; }
+            public object expiration { get; set; }
+            public object continuationToken { get; set; }
+            public Updates updates { get; set; }
+        }
+    }
+}
