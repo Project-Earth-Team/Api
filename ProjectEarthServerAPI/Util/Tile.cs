f228054138e4333b936b41e873480587235bc791
rtm516 <ryantmilner@hotmail.co.uk>
2021-02-27 14:00:56 +0000

    Move back to official CDN for files

--- ProjectEarthServerAPI/Util/Tile.cs
+++ ProjectEarthServerAPI/Util/Tile.cs
@@ -19,7 +19,8 @@ namespace ProjectEarthServerAPI.Util
             try
             {
                 Directory.CreateDirectory(Path.Combine(basePath, pos1.ToString()));
-                string downloadUrl = "https://tiles.projectearth.dev/styles/mc-earth/16/" + pos1 + "/" + pos2 + ".png";
+                string downloadUrl = "https://cdn.mceserv.net/tile/16/" + pos1 + "/" + pos1 + "_" + pos2 + "_16.png";
+                //string downloadUrl = "https://tiles.projectearth.dev/styles/mc-earth/16/" + pos1 + "/" + pos2 + ".png"; // Disabled until aliasing issues are fixed
                 webClient.DownloadFile(downloadUrl, Path.Combine(basePath, pos1.ToString(), $"{pos1}_{pos2}_16.png"));
                 webClient.Dispose();
                 return true;
