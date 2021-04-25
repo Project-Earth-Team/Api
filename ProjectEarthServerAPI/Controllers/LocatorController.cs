5b086eb6e992365f56be4cefcd36a4459921d155
Luminoso-256 <63971285+Luminoso-256@users.noreply.github.com>
2021-04-25 13:03:47 -0500

    Mojank :D

--- ProjectEarthServerAPI/Controllers/LocatorController.cs
+++ ProjectEarthServerAPI/Controllers/LocatorController.cs
@@ -16,7 +16,6 @@ namespace ProjectEarthServerAPI.Controllers
     [Route("player/environment")]
     public class LocatorController : Controller
     {
-
         private readonly ILogger<LocatorController> _logger;
 
         public LocatorController(ILogger<LocatorController> logger)
@@ -39,21 +38,69 @@ namespace ProjectEarthServerAPI.Controllers
                         {
                             playfabTitleId = "11509",
                             serviceUri = baseServerIP,
-                            cdnUri = baseServerIP+"/cdn",
+                            cdnUri = baseServerIP + "/cdn",
                             //playfabTitleId = "F0DE2" //maybe make our own soon? - Mojang could kill this anytime after server sunset with no warning. 
                         }
                     },
                     supportedEnvironments = new Dictionary<string, List<string>>()
                     {
-                        { "2020.1217.02", new List<string>() { "production" } }
+                        {"2020.1217.02", new List<string>() {"production"}}
                     }
                 },
                 //updates = new LocatorResponse.Updates()
             };
 
             var resp = JsonConvert.SerializeObject(response);
-            return Content(resp,"application/json");
+            return Content(resp, "application/json");
+        }
+    }
 
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("1.1")]
+    [Route("/api/v1.1/player/environment")]
+    //:mojanktriggered:
+    // Basically, the MCE gods decided peace was not an option
+    // so for some reason, some devices include the /api/v1.1, and some don't.
+    //  Hence this terrible thing.
+    public class MojankLocatorController : Controller
+    {
+        private readonly ILogger<MojankLocatorController> _logger;
+
+        public MojankLocatorController(ILogger<MojankLocatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ContentResult Get()
+        {
+            string baseServerIP = StateSingleton.Instance.config.baseServerIP;
+
+            LocatorResponse.Root response = new LocatorResponse.Root()
+            {
+                result = new LocatorResponse.Result()
+                {
+                    serviceEnvironments = new LocatorResponse.ServiceEnvironments()
+                    {
+                        production = new LocatorResponse.Production()
+                        {
+                            playfabTitleId = "11509",
+                            serviceUri = baseServerIP,
+                            cdnUri = baseServerIP + "/cdn",
+                            //playfabTitleId = "F0DE2" //maybe make our own soon? - Mojang could kill this anytime after server sunset with no warning. 
+                        }
+                    },
+                    supportedEnvironments = new Dictionary<string, List<string>>()
+                    {
+                        {"2020.1217.02", new List<string>() {"production"}}
+                    }
+                },
+                //updates = new LocatorResponse.Updates()
+            };
+
+            var resp = JsonConvert.SerializeObject(response);
+            return Content(resp, "application/json");
         }
     }
 }
